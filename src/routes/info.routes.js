bc33b34932545dd0669c90c97289ef1ffb1c736f
Gabriel Montoya <gabi.montoya@gmail.com>
2022-11-14 19:59:59 -0300

    DESAFIO-CLASE-32 Se agregaron los logs

--- src/routes/info.routes.js
+++ src/routes/info.routes.js
@@ -1,8 +1,24 @@
 import express from "express";
 import os from 'os';
+import compression from "compression";
 export const routerInfo = express.Router();
 
 routerInfo.get('/', async (req, res) => {
+    const { email: username } = await req.user;
+    const info = {
+        args: process.argv,
+        platform: process.platform,
+        pid: process.pid,
+        path: process.cwd(),
+        execPath: process.execPath,
+        nodeVersion: process.version,
+        reservedMemory: process.memoryUsage.rss(),
+        cpus: os.cpus().length
+    }
+    return res.render('info', {info, username});
+});
+
+routerInfo.get('/compressed', compression(), async (req, res) => {
     const { email: username } = await req.user;
     const info = {
         args: process.argv,
