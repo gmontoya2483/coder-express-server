3ec1062ef3ea80755233f9e4f454f29da39426cf
Gabriel Montoya <gabi.montoya@gmail.com>
2022-12-11 09:35:23 -0300

    DESAFIO-CLASE-38 Se borraron comentarios en randomRoutes

--- src/routes/api.randoms.routes.js
+++ src/routes/api.randoms.routes.js
@@ -6,12 +6,3 @@ export const routerApiRandoms = express.Router();
 
 
 routerApiRandoms.get('/', generateRandomNumbers);
-
-// routerApiRandoms.get('/', async (req, resp)=> {
-//     const forkedProcess = fork('./src/services/generateRandomsNumbers.js');
-//     const cant = (req.query.cant) ? parseInt(req.query.cant)  : 100000000;
-//     forkedProcess.send(cant);
-//     forkedProcess.on('message', response => {
-//         return resp.json(response);
-//     });
-// })
