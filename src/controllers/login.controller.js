87b2adf666a3e88e5cf3c8a047ad2b37f413b8c8
Gabriel Montoya <gabi.montoya@gmail.com>
2022-12-11 22:15:31 -0300

    DESAFIO-CLASE-38 Se completo capas Login

---
+++ src/controllers/login.controller.js
@@ -0,0 +1,8 @@
+
+export const renderLogin = (req, res) => {
+    return res.render('login')
+};
+
+export const renderLoginError = (req, res) => {
+    return res.render('login-error')
+};
---
+++ src/utils/passport.js
@@ -0,0 +1,38 @@
+import passport from "passport";
+import { Strategy } from "passport-local";
+// import {UsuariosDao} from "../daos/index.js";
+import {validateHash} from "./security.js";
+import {UsuarioService} from "../services/usuario.service.js";
+
+const LocalStrategy = Strategy;
+
+// const contenedor = new UsuariosDao();
+
+const usuarioService = new UsuarioService();
+
+passport.use(new LocalStrategy(
+    async function(username, password, done) {
+        // console.log(`${username} ${password}`)
+        //Logica para validar si un usuario existe
+        const existeUsuario = await usuarioService.getUserByUserName(username);
+        if (!existeUsuario)
+            return done(null, false);
+
+        const match = await validateHash(password, existeUsuario.password);
+        if (!match)
+            return done(null, false);
+
+        return done(null, existeUsuario);
+    }
+));
+
+passport.serializeUser((usuario, done)=>{
+    done(null, usuario.email);
+});
+
+passport.deserializeUser(async (email, done) => {
+    const existeUsuario = await usuarioService.getUserByUserName(email);
+    done(null, existeUsuario);
+});
+
+export default passport;
